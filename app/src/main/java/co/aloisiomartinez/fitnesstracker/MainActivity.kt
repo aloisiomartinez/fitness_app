85fa1a713787cb22ea9522448a25e12f2c6ec617
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-12-20 16:42:00 -0300

    starting with RecyclerView

--- app/src/main/java/co/aloisiomartinez/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/aloisiomartinez/fitnesstracker/MainActivity.kt
@@ -1,11 +1,13 @@
 package co.aloisiomartinez.fitnesstracker
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,48 +21,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1) o layout XML
-        // 2) a onde a recyclerview vai aparecer (tela principal, tela cheia)
-        // 3) logica - conectar o xml da celula DENTRO do recyclerView + a sua quantidade de elementos dinamicos
-        val adapter = MainAdapter()
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.ic_baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            )
+        )
+
+        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
         rvMain.layoutManager = LinearLayoutManager(this)
 
-        // classe para administar a recyclerview e suas celulas (os seus layouts de itens)
-        // Adapter ->
 
-//        btnImc = findViewById(R.id.btn_imc)
-//
-//        btnImc.setOnClickListener {
-//            // navegar para proxima tela
-//            val i = Intent(this, ImcActivity::class.java)
-//            startActivity(i)
-//        }
     }
 
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(private val mainItems: List<MainItem>) :
+        RecyclerView.Adapter<MainViewHolder>() {
 
-        // 1 - Qual é o layout XML da celula especifica (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
         }
 
-        // 2 - disparado toda vez houver uma rolagem na tela e for necessario trocar o conteudo
-        // da celula
+
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
-        // 3 - informar quantas celulas essa listagem terá
         override fun getItemCount(): Int {
-            return 30
+            return mainItems.size
         }
 
     }
 
-    // é a classe da celula em si!!!
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem){
+            val buttonTest: Button = itemView.findViewById(R.id.btn_item)
+            buttonTest.setText(item.textStringId)
+        }
     }
 
 }
